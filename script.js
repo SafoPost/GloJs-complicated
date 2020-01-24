b9200f0c37169e27b7b6af7578f6f09a7bc34d0c
Safo <s.i.post@mai.ru>
2020-01-24 21:35:56 +0500

    Lesson 2, compliceted

---
+++ script.js
@@ -0,0 +1,10 @@
+let num = 266219;
+let a = num.toString();
+let newNum = a[0] * a[1] * a[2] * a[3] * a[4] * a[5];
+
+console.log(newNum);
+
+newNum **= 3;
+b = newNum.toString();
+
+document.writeln(b[0], ' ', b[1]);
